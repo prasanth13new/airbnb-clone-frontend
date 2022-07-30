8f22e1ca8450e3253b98116a794d95d5bc5601c2
prasanth13new <prasanth13new@gmail.com>
2022-07-30 23:08:03 +0530

    logo linked to homepage

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import './navbar.css';
 
 function Navbar() {
     return (
         <div className='navbar'>
             <div className='navContainer'>
+                <Link to='/'>
                 <span className='logo'>Airbnb</span>
+                </Link>
                 <div className='navItem'>
                     <button className='navButton'>Register</button>
                     <button className='navButton'>Login</button>
