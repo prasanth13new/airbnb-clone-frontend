f5b675d04a3e0b54ab8b1a414d6615e1fbec9f09
prasanth13new <prasanth13new@gmail.com>
2022-11-30 01:03:30 +0530

    backend api

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -4,7 +4,8 @@ import './featured.css';
 
 function Featured() {
 
-    const { data, loading, error } = useFetch("http://localhost:8800/api/hotels/countByCity?cities=berlin,madrid,london,CA");
+    // const { data, loading, error } = useFetch("http://localhost:8800/api/hotels/countByCity?cities=berlin,madrid,london,CA");
+    const { data, loading, error } = useFetch("https://airbnb-clone-backend1.herokuapp.com/api/hotels/countByCity?cities=berlin,madrid,london,CA");
 
     const images=["https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=",
     "https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o=",
